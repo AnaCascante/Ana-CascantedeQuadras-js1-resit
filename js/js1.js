9824dd5b8a32a480151655ca7a2661c5b63af5d1
AnaCascante <ana.cascante@stud.noroff.no>
2022-12-29 23:42:33 +0100

    still don't get it!!!

--- js/js1.js
+++ js/js1.js
@@ -1,14 +1,38 @@
 
 const url ="https://api.noroff.dev/api/v1/jokes"; 
 
+const jokesContainer = document.querySelector (".jokes")
 
 /*
 const url ="https://noroffcors.onrender.com/noroff-api-docs.netlify.app/basic-endpoints/jokes#get-apiv1jokes";*/
 
 
 async function getJokes(){
-    const response = await fetch (url);
-    const results = response.json();
-  
-}
+    try{
+        const response = await fetch (url);
+        const results = await response.json();
+        const jokes = results;
+        console.log (jokes);
+
+        array.forEach(joke => {
+            resultContainer.innerHTML+= '<div class="jokes">
+                                        <h1>${joke.type}</h1>
+                                        <p> ${joke.setup}></p>
+                                        </div>'
+            
+        });
+    
+      
+    } catch (error){
+        console.log (error); 
+        resultContainer.innerHTML= message ("could't find the jokes")
+    }
+   
+
+    }
+
+      /*for (let i=0; i < jokes.length; i++){
+            console.log (jokes[i].text);
+            jokesContainer.innerHTML +='<div class="jokes">${jokes[i].text}</div>';
+        }*/
 getJokes();
