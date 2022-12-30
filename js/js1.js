3549ab85db156f134fa7256c464854656998b134
AnaCascante <ana.cascante@stud.noroff.no>
2022-12-30 21:14:32 +0100

    head out of order!!!
    I still need to get an undestanding of how to fetch the info from the api... its not appearing in my website!!! so frustrating!

--- js/js1.js
+++ js/js1.js
@@ -1,14 +1,64 @@
-
 const url ="https://api.noroff.dev/api/v1/jokes"; 
 
-const jokesContainer = document.querySelector (".jokes")
+const resultContainer = document.querySelector (".jokes")
+
+
+
+async function getJokes (){
+
+    try{
+    const response = await fetch (url);
+        const results = await response.json();
+        console.log (results);
+     
+        resultContainer.innerHTML="";
+        for (let i=0; i<results.length; i++){
+            resultContainer.innerHTML +=
+            '<div class="jokes">${result[i].type} +" " + ${result[i].setup}</div>'
+        }
+        
+
+
+    }catch (error){
+        console.log (error);
+        resultContainer.innerHTML= message ("Something went wrong!")
+    }
+
+}
+
+getJokes ();
+
+
+
+
+/*async function makeApiCall(){
+    try{
+        const response = await fetch (url);
+        const results = await response.json();
+        const jokes = results;
+        console.log (jokes);
+        
+}*/
+
+
+
+/* this is not working!
+    for (let i=0;i<jokes.length;i++){
+        if (!jokes[i].type){
+            continue;
+        }
+    } catch (error){
+        console.log(error)
+        jokesContainer.innerHTML = message ("Something went wrong!")
+    }*/
+
 
 /*
 const url ="https://noroffcors.onrender.com/noroff-api-docs.netlify.app/basic-endpoints/jokes#get-apiv1jokes";*/
 
 
-async function getJokes(){
-    try{
+/*async function getJokes(){
+    /*try{
         const response = await fetch (url);
         const results = await response.json();
         const jokes = results;
@@ -17,22 +67,33 @@ async function getJokes(){
         array.forEach(joke => {
             resultContainer.innerHTML+= '<div class="jokes">
                                         <h1>${joke.type}</h1>
-                                        <p> ${joke.setup}></p>
+                                        <p>${joke.setup}></p>
                                         </div>'
             
-        });
-    
-      
+        }); 
     } catch (error){
         console.log (error); 
         resultContainer.innerHTML= message ("could't find the jokes")
     }
-   
+    }*/
 
-    }
-
-      /*for (let i=0; i < jokes.length; i++){
+     /* for (let i=0; i < jokes.length; i++){
             console.log (jokes[i].text);
             jokesContainer.innerHTML +='<div class="jokes">${jokes[i].text}</div>';
-        }*/
-getJokes();
+        }
+
+getJokes ();*/
+
+/*async function getJokes (){
+    const response =await fetch (url);
+    const result = await response.json();
+    const jokes = results;
+    console.log (jokes);
+
+    result.forEach (function (jokes)){
+        resultsContainer.innerHTML += 
+        '<div class="jokes>
+            <h1>${jokes.type}</h1>
+            <p>${jokes.setup}</p>'
+    }}*/
+    
